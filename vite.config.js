2a2f4cff203101919c18afda8a1fb98828a8b0a3
jd164 <joaodionildo@gmail.com>
2026-08-18 18:32:44 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Permite rodar em subpastas como nome-do-repositorio no GitHub Pages
+  base: '/calculo-rapido/',
   server: {
     port: 3000,
     open: true
